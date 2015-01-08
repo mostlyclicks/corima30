f45a0c229df72d0517b2e53a47dd3987b43087ba
Carlos Interone <carlos@mostlyclicks.com>
2015-01-08 14:23:34 -0600

    set up for refinery s3 uploads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Corima30::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # AWS
+  config.s3_backend = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
