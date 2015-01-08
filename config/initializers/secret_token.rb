a0420be66e583f2c5199c42b4f8d5ee70246f11f
Carlos Interone <carlos@mostlyclicks.com>
2015-01-08 09:55:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corima30::Application.config.secret_token = '38fd55ea782e36791ee5536aa33b49c1a7a0d3b4831a6d8e88fdfa3a64419a2bedd2375fe9a7b00012a5b4d5f52b59bf0419d2c08cc17ab46bb4f1377caad330'
