30f1dc14395da85b4f58c44b89a72ce55eb4e7a7
Carlos Interone <carlos@mostlyclicks.com>
2015-01-13 09:46:28 -0600

    start show design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def get_cat_wheels(h)
+      case
+        when h == 'MCC S+'
+          @cat_wheels = @mcc_splus_wheels
+        when h == 'S+'
+          @cat_wheels = @splus_wheels
+        when h == 'S'
+          @cat_wheels = @s_wheels
+        when h == ''
+          @cat_wheels = @no_s_wheels
+        else
+          puts '<p>No other wheels here.</p>'
+      end
+    end
 end
