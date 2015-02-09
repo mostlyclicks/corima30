8fee2d52a2a488ef879c7535855a12d1390f399c
Carlos Interone <carlos@mostlyclicks.com>
2015-02-09 12:09:00 -0600

    add cat home image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :find_mcc_splus
+  before_filter :find_random_mcc_splus
+
   before_filter :find_s_plus
+  before_filter :find_random_s_plus
+
   before_filter :find_s
+  before_filter :find_random_s
+
   before_filter :find_no_s
+  before_filter :find_random_no_s
 
 
 
@@ -14,15 +21,31 @@ class ApplicationController < ActionController::Base
     @mcc_splus_wheels = Refinery::Wheels::Wheel.where(hub_type: 'MCC S+').order('profile asc')
   end
 
+  def find_random_mcc_splus
+    @mcc_random = @mcc_splus_wheels.sample
+  end
+
   def find_s_plus
     @splus_wheels = Refinery::Wheels::Wheel.where(hub_type: 'S+').order('profile asc')
+  end
+
+  def find_random_s_plus
+    @splus_random = @splus_wheels.sample
   end
 
   def find_s
     @s_wheels = Refinery::Wheels::Wheel.where(hub_type: 'S').order('profile asc')
   end
 
+  def find_random_s
+    @s_random = @s_wheels.sample
+  end
+
   def find_no_s
     @no_s_wheels = Refinery::Wheels::Wheel.where(hub_type: '').order('profile asc')
+  end
+
+  def find_random_no_s
+    @no_s_random = @no_s_wheels.sample
   end
 end
