d46d17e70aa1d81bf134e89f46c6f822358a17fb
Carlos Interone <carlos@mostlyclicks.com>
2015-02-10 09:21:10 -0600

    change sendgrid to herokuapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Corima30::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 
